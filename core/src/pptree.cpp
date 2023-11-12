b07cdfb2f524a1846c0845116577bc8643638ce1
Andrés Vidal <andres@wyeworks.com>
2023-11-12 20:33:35 -0300

    Remove placeholder code
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/pptree.cpp
+++ core/src/pptree.cpp
@@ -2,10 +2,4 @@
 
 #include <iostream>
 
-#include "linear_algebra.hpp"
-
-namespace pptree {
-void fit() {
-  std::cout << "Hello, World!" << " FYI, 1 + 1 is " << sum(1, 1) << std::endl;
-}
-}
+namespace pptree {}
