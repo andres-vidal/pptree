924488174f9db3f8f1847db4ff34d807c31877f2
Andrés Vidal <andres@wyeworks.com>
2023-12-03 20:36:28 -0300

    Add matricial versions of inner_product and inner_squareÃ
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/stats.cpp
+++ core/src/stats.cpp
@@ -55,6 +55,20 @@ double inner_square(
   return inner_product(a, a, weights);
 }
 
+DMatrix<double> inner_product(
+  DMatrix<double> a,
+  DMatrix<double> b,
+  DMatrix<double> weights) {
+  return (a.transpose() * weights * b);
+}
+
+DMatrix<double> inner_square(
+  DMatrix<double> a,
+  DMatrix<double> weights
+  ) {
+  return inner_product(a, a, weights);
+}
+
 DMatrix<double> between_groups_sum_of_squares(
   DMatrix<double>         data,
   DVector<unsigned short> groups,
