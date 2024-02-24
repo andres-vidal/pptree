980711794471ae0c717e150cbed8887ffaf660d8
Andrés Vidal <andres@wyeworks.com>
2024-02-24 18:55:23 -0300

    Replace custom Rcpp Exporters with 'as' overloads
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- bindings/R/PPTree/inst/include/PPTree.h
+++ bindings/R/PPTree/inst/include/PPTree.h
@@ -8,13 +8,13 @@ namespace Rcpp {
   SEXP wrap(const pptree::Tree<long double, int> &tree);
   SEXP wrap(const pptree::Response<long double, int> &node);
   SEXP wrap(const pptree::Condition<long double, int> &node);
+
+  template<> std::unique_ptr<pptree::Node<long double, int> > as(SEXP);
+  template<> pptree::Tree<long double, int> as(SEXP);
+  template<> pptree::Response<long double, int> as(SEXP);
+  template<> pptree::Condition<long double, int> as(SEXP);
 }
 
-namespace Rcpp::traits {
-  template<> class Exporter<pptree::Response<long double, int> >;
-  template<> class Exporter<pptree::Condition<long double, int> >;
-  template<> class Exporter<pptree::Tree<long double, int> >;
-}
 
 #include <Rcpp.h>
 
@@ -22,9 +22,9 @@ namespace Rcpp {
   SEXP wrap(const pptree::Node<long double, int>& node) {
     if (node.is_response()) {
       return wrap(node.as_response());
-    } else {
-      return wrap(node.as_condition());
     }
+
+    return wrap(node.as_condition());
   }
 
   SEXP wrap(const pptree::Response<long double, int> &node) {
@@ -44,60 +44,45 @@ namespace Rcpp {
     return Rcpp::List::create(
       Rcpp::Named("root") = Rcpp::wrap(*tree.root));
   }
-}
 
-namespace Rcpp::traits {
-  template<> class Exporter<pptree::Condition<long double, int> > {
-    Rcpp::List rnode;
+  template<> std::unique_ptr<pptree::Node<long double, int> > as(SEXP x) {
+    Rcpp::List rnode(x);
 
-    public:
-      Exporter(SEXP x) : rnode(x) {
-      }
+    if (rnode.containsElementNamed("value")) {
+      auto resp = as<pptree::Response<long double, int> >(x);
+      auto resp_ptr = std::make_unique<pptree::Response<long double, int> >(std::move(resp));
+      return std::move(resp_ptr);
+    }
 
-      pptree::Condition<long double, int> get() {
-        auto projector = Rcpp::as<pptree::pp::Projector<long double> >(rnode["projector"]);
-        auto threshold = Rcpp::as<pptree::Threshold<long double> >(rnode["threshold"]);
+    auto cond = as<pptree::Condition<long double, int> >(x);
+    auto cond_ptr = std::make_unique<pptree::Condition<long double, int> >(std::move(cond));
+    return std::move(cond_ptr);
+  }
 
-        auto lower_exporter = Exporter<pptree::Condition<long double, int> >(rnode["upper"]);
-        auto lower_node = std::make_unique<pptree::Condition<long double, int> >(lower_exporter.get());
+  template<> pptree::Response<long double, int> as(SEXP x) {
+    Rcpp::List rresp(x);
+    return pptree::Response<long double, int>(Rcpp::as<long double>(rresp["value"]));
+  }
 
+  template<> pptree::Condition<long double, int> as(SEXP x) {
+    Rcpp::List rcond(x);
 
-        auto upper_exporter = Exporter<pptree::Condition<long double, int> >(rnode["lower"]);
-        auto upper_node = std::make_unique<pptree::Condition<long double, int> >(upper_exporter.get());
+    auto lower = as<std::unique_ptr<pptree::Node<long double, int> > >(rcond["lower"]);
+    auto upper = as<std::unique_ptr<pptree::Node<long double, int> > >(rcond["upper"]);
 
-        return pptree::Condition<long double, int>(
-          projector,
-          threshold,
-          std::move(lower_node),
-          std::move(upper_node));
-      };
-  };
+    return pptree::Condition<long double, int>(
+      Rcpp::as<pptree::Projector<long double> >(rcond["projector"]),
+      Rcpp::as<long double>(rcond["threshold"]),
+      std::move(lower),
+      std::move(upper));
+  }
 
-  template<> class Exporter<pptree::Response<long double, int> > {
-    Rcpp::List rnode;
+  template<> pptree::Tree<long double, int> as(SEXP x) {
+    Rcpp::List rtree(x);
 
-    public:
-      Exporter(SEXP x) : rnode(x) {
-      }
+    auto root = as<pptree::Condition<long double, int> >(rtree["root"]);
+    auto root_ptr = std::make_unique<pptree::Condition<long double, int> >(std::move(root));
 
-      pptree::Response<long double, int> get() {
-        auto value = Rcpp::as<int>(rnode["value"]);
-        return pptree::Response<long double, int>(value);
-      }
-  };
-
-  template<> class Exporter<pptree::Tree<long double, int> > {
-    Rcpp::List rtree;
-
-    public:
-      Exporter(SEXP x) : rtree(x) {
-      }
-
-      pptree::Tree<long double, int> get() {
-        auto root_exporter = Exporter<pptree::Condition<long double, int> >(rtree["root"]);
-        auto root_node = std::make_unique<pptree::Condition<long double, int> >(root_exporter.get());
-
-        return pptree::Tree<long double, int>(std::move(root_node));
-      }
-  };
+    return pptree::Tree<long double, int>(std::move(root_ptr));
+  }
 }
