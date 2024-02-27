78c7f73e4752aeac4ccd4815238b9b509c262fcc
Andrés Vidal <andres@wyeworks.com>
2024-02-27 15:49:39 -0300

    Add automatic data preprocessing
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- bindings/R/PPTree/R/PPTree.R
+++ bindings/R/PPTree/R/PPTree.R
@@ -8,38 +8,48 @@ NULL
 #' @param y A matrix containing the labels for each observation.
 #' @return A PPTree model trained on \code{x} and \code{y}.
 #' @examples
-#' # Example 1: with the preprocessed `iris` dataset
-#' library(PPTree)
-#' PPTree(x = PPTreeIris[, 1:4], y = PPTreeIris[, 5])
+#' # Example 1: with the `iris` dataset
+#' PPTree(x = iris[, 1:4], y = iris[, 5])
 #'
-#' # Example 2: with the preprocessed `crabs` dataset
-#' library(PPTree)
-#' PPTree(x = PPTreeCrabs[, 2:7], y = PPTreeCrabs[, 1])
+#' # Example 2: with the `crabs` dataset
+#' x <- crabs[, c(2, 4:8)]
+#' x$sex <- as.numeric(as.factor(x$sex))
+#' PPTree(x = x, y = crabs[, 1])
 #'
 #' @export
 PPTree <- function(x, y) {
-  model <- pptree_train_lda(as.matrix(x), as.matrix(y))
+  if (!all(sapply(x, is.numeric))) {
+    stop("All columns in x must be numeric")
+  }
+
+  if (!is.factor(y)) {
+    y <- factor(y)
+  }
+  model <- pptree_train_lda(as.matrix(x), as.matrix(as.numeric(y)))
   class(model) <- "PPTree"
+  model$classes <- levels(y)
+
   model
 }
 
 #' Predicts the labels of a set of observations using a PPTree model.
-#' 
+#'
 #' @param model A PPTree model.
 #' @param x A matrix containing the features for each new observation.
 #' @return A matrix containing the predicted labels for each observation.
 #' @examples
-#' # Example 1: with the preprocessed `iris` dataset
-#' library(PPTree)
-#' model <- PPTree(PPTreeIris[, 1:4], PPTreeIris[, 5])
-#' predict(model, PPTreeIris[, 1:4])
+#' # Example 1: with the `iris` dataset
+#' model <- PPTree(iris[, 1:4], iris[, 5])
+#' predict(model, iris[, 1:4])
 #'
-#' # Example 2: with the preprocessed `crabs` dataset
-#' library(PPTree)
-#' model <- PPTree(PPTreeCrabs[, 2:7], PPTreeCrabs[, 1])
-#' predict(model, PPTreeCrabs[, 2:7])
+#' # Example 2: with the `crabs` dataset
+#' x <- crabs[, c(2, 4:8)]
+#' x$sex <- as.numeric(as.factor(x$sex))
+#' model <- PPTree(x = x, y = crabs[, 1])
+#' predict(model, x)
 #'
 #' @export
 predict.PPTree <- function(model, x) {
-  pptree_predict(model, as.matrix(x))
+  y <- pptree_predict(model, as.matrix(x))
+  as.factor(model$classes[y])
 }
