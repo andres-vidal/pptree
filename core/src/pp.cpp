63b0179fef5f24df1c0c788387d4df49c3b0bfbf
Andrés Vidal <andres@wyeworks.com>
2024-03-20 23:15:55 -0300

    Merge pp::lda_* and pp::pda_* into pp:glda_*
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/pp.cpp
+++ core/src/pp.cpp
@@ -26,89 +26,7 @@ namespace pp {
   }
 
   template<typename T, typename G>
-  Projector<T> lda_optimum_projector(
-    const Data<T> &      data,
-    const DataColumn<G> &groups,
-    const std::set<G> &  unique_groups) {
-    LOG_INFO << "Calculating LDA optimum projector for " << unique_groups.size() << " groups: " << unique_groups << std::endl;
-    LOG_INFO << "Dataset size: " << data.rows() << " observations of " << data.cols() << " variables:" << std::endl;
-    LOG_INFO << std::endl << data << std::endl;
-    LOG_INFO << "Groups:" << std::endl;
-    LOG_INFO << std::endl << groups << std::endl;
-
-    Data<T> W = within_groups_sum_of_squares(data, groups, unique_groups);
-    Data<T> B = between_groups_sum_of_squares(data, groups, unique_groups);
-    Data<T> WpB = W + B;
-
-    LOG_INFO << "W:" << std::endl << W << std::endl;
-    LOG_INFO << "B:" << std::endl << B << std::endl;
-    LOG_INFO << "W + B:" << std::endl << WpB << std::endl;
-
-    Data<T> WpBInvB = solve(WpB, B);
-    Data<T> truncatedWpBInvB = WpBInvB.unaryExpr((T (*)(T)) & truncate_op);
-
-    LOG_INFO << "(W + B)^-1 * B:" << std::endl << WpBInvB << std::endl;
-    LOG_INFO << "(W + B)^-1 * B (truncated):" << std::endl << truncatedWpBInvB << std::endl;
-
-    auto [eigen_val, eigen_vec] = linalg::eigen(truncatedWpBInvB);
-
-    LOG_INFO << "Eigenvalues:" << std::endl << eigen_val << std::endl;
-    LOG_INFO << "Eigenvectors:" << std::endl << eigen_vec << std::endl;
-
-    Projector<T> projector = get_projector(eigen_vec);
-
-    LOG_INFO << "Projector:" << std::endl << projector << std::endl;
-    return projector;
-  }
-
-  template Projector<long double> lda_optimum_projector<long double, int>(
-    const Data<long double> & data,
-    const DataColumn<int> &   groups,
-    const std::set<int> &     unique_groups);
-
-  template<typename T, typename G>
-  T lda_index(
-    const Data<T> &      data,
-    const Projector<T> & projector,
-    const DataColumn<G> &groups,
-    const std::set<G> &  unique_groups) {
-    Data<T> A = projector;
-
-    Data<T> W = within_groups_sum_of_squares(data, groups, unique_groups);
-    Data<T> B = between_groups_sum_of_squares(data, groups, unique_groups);
-
-    T denominator = determinant(inner_square(A, W + B));
-
-    if (denominator == 0) {
-      return 0;
-    }
-
-    return 1 - determinant(inner_square(A, W)) / denominator;
-  }
-
-  template long double lda_index<long double, int>(
-    const Data<long double> &     data,
-    const Projector<long double> &projector,
-    const DataColumn<int> &       groups,
-    const std::set<int> &         unique_groups);
-
-  template<typename T, typename G>
-  PPStrategyReturn<T> lda_strategy(
-    const Data<T> &      data,
-    const DataColumn<G> &groups,
-    const std::set<G> &  unique_groups) {
-    Projector<T> projector = lda_optimum_projector(data, groups, unique_groups);
-    return { projector, project(data, projector) };
-  }
-
-  template PPStrategyReturn<long double> lda_strategy<long double, int>(
-    const Data<long double> & data,
-    const DataColumn<int> &   groups,
-    const std::set<int> &     unique_groups);
-
-
-  template<typename T, typename G>
-  Projector<T> pda_optimum_projector(
+  Projector<T> glda_optimum_projector(
     const Data<T> &      data,
     const DataColumn<G> &groups,
     const std::set<G> &  unique_groups,
@@ -147,7 +65,7 @@ namespace pp {
     return projector;
   }
 
-  template Projector<long double> pda_optimum_projector<long double, int>(
+  template Projector<long double> glda_optimum_projector<long double, int>(
     const Data<long double> & data,
     const DataColumn<int> &   groups,
     const std::set<int> &     unique_groups,
@@ -155,7 +73,7 @@ namespace pp {
 
 
   template<typename T, typename G>
-  T pda_index(
+  T glda_index(
     const Data<T> &      data,
     const Projector<T> & projector,
     const DataColumn<G> &groups,
@@ -177,7 +95,7 @@ namespace pp {
     return 1 - determinant(inner_square(A, W_pda)) / denominator;
   }
 
-  template long double pda_index<long double, int>(
+  template long double glda_index<long double, int>(
     const Data<long double> &     data,
     const Projector<long double> &projector,
     const DataColumn<int> &       groups,
@@ -185,15 +103,15 @@ namespace pp {
     const double                  lambda);
 
   template<typename T, typename G>
-  PPStrategy<T, G> pda_strategy(
+  PPStrategy<T, G> glda_strategy(
     const double lambda) {
     return [lambda](const Data<T>& data, const DataColumn<G>& groups, const std::set<G>& unique_groups) -> PPStrategyReturn<T> {
-             auto projector = pda_optimum_projector(data, groups, unique_groups, lambda);
+             auto projector = glda_optimum_projector(data, groups, unique_groups, lambda);
              return (PPStrategyReturn<T>) { projector, project(data, projector) };
     };
   }
 
-  template PPStrategy<long double, int> pda_strategy<long double, int>(
+  template PPStrategy<long double, int> glda_strategy<long double, int>(
     const double lambda);
 
   template<typename T>
