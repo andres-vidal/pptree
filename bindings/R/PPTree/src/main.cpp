e84fc5ba4f0c1b26d662c4e7c26955ff84120db4
Andrés Vidal <andres@wyeworks.com>
2024-03-27 01:34:17 -0300

    Add Rcpp interface definitions for Forest
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- bindings/R/PPTree/src/main.cpp
+++ bindings/R/PPTree/src/main.cpp
@@ -1,13 +1,12 @@
 #include <Rcpp.h>
 #include <RcppEigen.h>
 #include "pptree.hpp"
+#include <random>
 
 // [[Rcpp::depends(RcppEigen)]]
 
 using namespace Rcpp;
 using namespace RcppEigen;
-
-
 
 // [[Rcpp::export]]
 pptree::Tree<long double, int> pptree_train_glda(
@@ -18,8 +17,26 @@ pptree::Tree<long double, int> pptree_train_glda(
 }
 
 // [[Rcpp::export]]
+pptree::Forest<long double, int> pptree_train_forest_glda(
+  const pptree::Data<long double> & data,
+  const pptree::DataColumn<int> &   groups,
+  const int                         size,
+  const int                         n_vars,
+  const double                      lambda) {
+  std::mt19937 gen(R::rnorm(0, 1));
+  return pptree::train_forest_glda(data, groups, size, n_vars, lambda, gen);
+}
+
+// [[Rcpp::export]]
 pptree::DataColumn<int> pptree_predict(
   pptree::Tree<long double, int> tree,
   pptree::Data<long double>      data) {
   return tree.predict(data);
+}
+
+// [[Rcpp::export]]
+pptree::DataColumn<int> pptree_predict_forest(
+  pptree::Forest<long double, int> forest,
+  pptree::Data<long double>        data) {
+  return forest.predict(data);
 }
