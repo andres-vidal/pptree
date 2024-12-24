04691e39e97c9b5d0f605cfec939c55c30598607
Andrés Vidal <andres@wyeworks.com>
2024-12-24 11:54:02 -0300

    Add tests
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/SortedDataSpec.test.cpp
+++ core/src/SortedDataSpec.test.cpp
@@ -148,3 +148,160 @@ TEST(SortedDataSpec, Group) {
   ASSERT_EQ(expected.cols(), actual.cols());
   ASSERT_EQ(expected, actual);
 }
+
+TEST(SortedDataSpec, Analog) {
+  Data<double> x(6, 3);
+  x <<
+    1, 1, 1,
+    2, 2, 2,
+    3, 3, 3,
+    4, 4, 4,
+    5, 5, 5,
+    6, 6, 6;
+
+  DataColumn<int> y(6);
+  y <<
+    2,
+    1,
+    3,
+    1,
+    3,
+    2;
+
+  SortedDataSpec<double, int> data(x, y);
+
+  Data<double> new_x(6, 3);
+
+  new_x <<
+    2, 2, 2,
+    4, 4, 4,
+    1, 1, 1,
+    6, 6, 6,
+    3, 3, 3,
+    5, 5, 5;
+
+  SortedDataSpec<double, int> actual = data.analog(new_x);
+
+  ASSERT_EQ(new_x.size(), actual.x.size());
+  ASSERT_EQ(new_x.rows(), actual.x.rows());
+  ASSERT_EQ(new_x.cols(), actual.x.cols());
+  ASSERT_EQ(new_x, actual.x);
+
+  ASSERT_EQ(data.y.size(), actual.y.size());
+  ASSERT_EQ(data.y.rows(), actual.y.rows());
+  ASSERT_EQ(data.y.cols(), actual.y.cols());
+  ASSERT_EQ(data.y, actual.y);
+
+  ASSERT_EQ(data.classes, actual.classes);
+}
+
+TEST(SortedDataSpec, Remap) {
+  Data<double> x(6, 3);
+  x <<
+    1, 1, 1,
+    1, 2, 2,
+    2, 1, 1,
+    2, 2, 2,
+    3, 1, 1,
+    3, 2, 2;
+
+  DataColumn<int> y(6);
+  y <<
+    1,
+    1,
+    2,
+    2,
+    3,
+    3;
+
+  SortedDataSpec<double, int> data(x, y);
+
+  std::map<int, int> mapping = {
+    { 1, 0 },
+    { 2, 1 },
+    { 3, 0 }
+  };
+
+  SortedDataSpec<double, int> actual = data.remap(mapping);
+
+  Data<double> new_x(6, 3);
+  new_x <<
+    1, 1, 1,
+    1, 2, 2,
+    3, 1, 1,
+    3, 2, 2,
+    2, 1, 1,
+    2, 2, 2;
+
+
+  DataColumn<int> new_y(6);
+  new_y <<
+    0,
+    0,
+    0,
+    0,
+    1,
+    1;
+
+  ASSERT_EQ(new_x.size(), actual.x.size());
+  ASSERT_EQ(new_x.rows(), actual.x.rows());
+  ASSERT_EQ(new_x.cols(), actual.x.cols());
+  ASSERT_EQ(new_x, actual.x);
+
+  ASSERT_EQ(new_y.size(), actual.y.size());
+  ASSERT_EQ(new_y.rows(), actual.y.rows());
+  ASSERT_EQ(new_y.cols(), actual.y.cols());
+  ASSERT_EQ(new_y, actual.y);
+
+  ASSERT_EQ(std::set<int>({ 0, 1 }), actual.classes);
+}
+
+TEST(SortedDataSpec, Subset) {
+  Data<double> x(6, 3);
+  x <<
+    1, 1, 1,
+    1, 2, 2,
+    2, 1, 1,
+    2, 2, 2,
+    3, 1, 1,
+    3, 2, 2;
+
+  DataColumn<int> y(6);
+  y <<
+    1,
+    1,
+    2,
+    2,
+    3,
+    3;
+
+  SortedDataSpec<double, int> data(x, y);
+
+  SortedDataSpec<double, int> actual = data.subset({ 1, 3 });
+
+  Data<double> new_x(4, 3);
+  new_x <<
+    1, 1, 1,
+    1, 2, 2,
+    3, 1, 1,
+    3, 2, 2;
+
+  DataColumn<int> new_y(4);
+  new_y <<
+    1,
+    1,
+    3,
+    3;
+
+  ASSERT_EQ(new_x.size(), actual.x.size());
+  ASSERT_EQ(new_x.rows(), actual.x.rows());
+  ASSERT_EQ(new_x.cols(), actual.x.cols());
+  ASSERT_EQ(new_x, actual.x);
+
+  ASSERT_EQ(new_y.size(), actual.y.size());
+  ASSERT_EQ(new_y.rows(), actual.y.rows());
+  ASSERT_EQ(new_y.cols(), actual.y.cols());
+  ASSERT_EQ(new_y, actual.y);
+
+  ASSERT_EQ(std::set<int>({ 1, 3 }), actual.classes);
+}
