12b98e7a080a0941d15b227bf693465463a51e56
Andrés Vidal <andresvidalberriel@gmail.com>
2025-05-17 00:03:24 -0300

    Remove unused functions
    Signed-off-by: Andrés Vidal <andresvidalberriel@gmail.com>

--- core/src/Map.hpp
+++ core/src/Map.hpp
@@ -2,25 +2,9 @@
 
 #include <vector>
 #include <map>
+#include <set>
 
 namespace utils {
-  template<typename K, typename V>
-  std::vector<K> sort_keys_by_value(const std::map<K, V> &mapping) {
-    std::vector<std::pair<K, V> > items(mapping.begin(), mapping.end());
-
-    std::sort(items.begin(), items.end(), [](const auto &a, const auto &b) {
-        return a.second < b.second || (a.second == b.second && a.first < b.first);
-      });
-
-    std::vector<K> result;
-    result.reserve(items.size());
-    for (auto &kv : items) {
-      result.push_back(kv.first);
-    }
-
-    return result;
-  }
-
   template<typename K, typename V>
   std::map<V, std::set<K> > invert(const std::map<K, V> &map) {
     std::map<V, std::set<K> > result;
