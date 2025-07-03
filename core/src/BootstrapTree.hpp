155f42791a2bcac373adf6565e13f257481056ff
Andrés Vidal <andres@wyeworks.com>
2025-07-02 23:49:44 -0300

    Rename iob_indices to sample_indices
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/BootstrapTree.hpp
+++ core/src/BootstrapTree.hpp
@@ -19,8 +19,8 @@ namespace models {
       const stats::Data<T> &     x,
       const stats::GroupSpec<R> &group_spec,
       stats::RNG &               rng) {
-      std::vector<int> iob_indices;
-      iob_indices.reserve(x.rows());
+      std::vector<int> sample_indices;
+      sample_indices.reserve(x.rows());
 
       for (const auto& group : group_spec.groups) {
         const int group_size = group_spec.group_size(group);
@@ -30,36 +30,36 @@ namespace models {
         const stats::Uniform unif(min_index, max_index);
 
         for (int j = 0; j < group_size; j++) {
-          iob_indices.push_back(unif(rng));
+          sample_indices.push_back(unif(rng));
         }
       }
 
-      std::sort(iob_indices.begin(), iob_indices.end());
+      std::sort(sample_indices.begin(), sample_indices.end());
 
-      stats::Data<T> sampled_x = x(iob_indices, Eigen::all);
+      stats::Data<T> sampled_x = x(sample_indices, Eigen::all);
 
       Tree<T, R> tree = Tree<T, R>::train(training_spec, sampled_x, group_spec, rng);
 
       return std::make_unique<BootstrapTree<T, R> >(
         std::move(tree.root),
         training_spec.clone(),
-        iob_indices);
+        sample_indices);
     }
 
-    std::vector<int> iob_indices;
+    std::vector<int> sample_indices;
 
     BootstrapTree(
       TreeNodePtr<T, R> root) :
       Base(std::move(root)),
-      iob_indices() {
+      sample_indices() {
     }
 
     BootstrapTree(
       TreeNodePtr<T, R>        root,
       TrainingSpecPtr<T, R>    training_spec,
-      const std::vector<int> & iob_indices) :
+      const std::vector<int> & sample_indices) :
       Base(std::move(root), std::move(training_spec)),
-      iob_indices(iob_indices) {
+      sample_indices(sample_indices) {
     }
   };
 }
