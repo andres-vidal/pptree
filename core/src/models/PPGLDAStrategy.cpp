e06a7232f7fa7a78ff34f1e6e03d60afedda9ef7
Andrés Vidal <andres@wyeworks.com>
2026-03-11 16:34:44 -0300

    Avoid inverse in PPGLDAStrategy
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- core/src/models/PPGLDAStrategy.cpp
+++ core/src/models/PPGLDAStrategy.cpp
@@ -47,45 +47,20 @@ namespace ppforest2::pp {
   PPResult PPGLDAStrategy::optimize(
     const FeatureMatrix&  x,
     const GroupPartition& group_spec) const {
-    FeatureMatrix B = group_spec.bgss(x);
-    FeatureMatrix W = group_spec.wgss(x);
+    const FeatureMatrix B = group_spec.bgss(x);
+    const FeatureMatrix W = group_spec.wgss(x);
 
-    FeatureMatrix W_pda = (1 - lambda) * W;
+    FeatureMatrix W_pda = (Feature(1) - Feature(lambda)) * W;
     W_pda.diagonal() = W.diagonal();
 
-    FeatureMatrix WpB = W_pda + B;
+    const FeatureMatrix WpB = W_pda + B; // symmetric
 
-    FeatureMatrix WpBInvB = WpB.fullPivLu().solve(B);
+    Eigen::GeneralizedSelfAdjointEigenSolver<FeatureMatrix> ges;
+    ges.compute(B, WpB);
 
-    Eigen::EigenSolver<FeatureMatrix> eigen_solver(WpBInvB);
-    FeatureVector eigen_val = eigen_solver.eigenvalues().real();
-    FeatureMatrix eigen_vec = eigen_solver.eigenvectors().real();
-
-    std::vector<Eigen::Index> indices(static_cast<std::size_t>(eigen_val.size()));
-    std::iota(indices.begin(), indices.end(), 0);
-
-    std::sort(indices.begin(), indices.end(),
-      [&eigen_val, &eigen_vec](Eigen::Index idx1, Eigen::Index idx2) {
-        float value1_mod = std::fabs(eigen_val(idx1));
-        float value2_mod = std::fabs(eigen_val(idx2));
-
-        if (math::is_approx(value1_mod, value2_mod, 0.001f)) {
-          FeatureVector vector1 = eigen_vec.col(idx1);
-          FeatureVector vector2 = eigen_vec.col(idx2);
-
-          for (Eigen::Index i = 0; i < vector1.size(); ++i) {
-            if (!math::is_module_approx(vector1[i], vector2[i])) {
-              return vector1[i] < vector2[i];
-            }
-          }
-        }
-
-        return value1_mod < value2_mod;
-      });
-
-    FeatureVector max_eigen_vec = eigen_vec.col(indices.back());
-    Feature max_eigen_val       = eigen_val(indices.back());
-
+    // largest eigenvalue => best 1D projection
+    FeatureVector max_eigen_vec = ges.eigenvectors().col(ges.eigenvalues().size() - 1);
+    Feature max_eigen_val       = ges.eigenvalues().real().maxCoeff();
     return PPResult{ pp::normalize(max_eigen_vec), max_eigen_val };
   }
 
