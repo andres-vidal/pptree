c8011bc1f8751a1cb4dab78285ca7ac2b5b5cfef
andres-vidal <andres-vidal@users.noreply.github.com>
2026-03-23 01:10:24 +0000

    deploy: dbcfcb680f8da51d49e75402a4888bc2e1e16693

--- main/cpp/navtreedata.js
+++ main/cpp/navtreedata.js
@@ -98,8 +98,8 @@ var NAVTREEINDEX =
 "functions_m.html",
 "structppforest2_1_1Forest.html#a779fbf1d1dbdd1a808841a8b7e1ebf7a",
 "structppforest2_1_1cli_1_1BenchmarkParams.html#aece25fda7ac99aa87a5f0463a95b836e",
-"structppforest2_1_1io_1_1ModelStats.html#abe5139d459e9d907ed068cad5cce849f",
-"structppforest2_1_1viz_1_1NodeData.html#ace58e17809ab9eaddad15054fda49529"
+"structppforest2_1_1io_1_1ModelStats.html#aa2a7bcfb4ba470aba4e3e95ee0afd1e4",
+"structppforest2_1_1viz_1_1NodeData.html#abefd12391d8f7be6525933d855f07f31"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronization';
